7a4246478bc446bd14ac884d4e5a145f73295e0d
dnwhd0112 <dnwhd0112@naver.com>
2022-04-13 21:35:10 +0900

    feat: 첫번쨰화면 구현

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -8,11 +8,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var parisImageView: UIImageView!
+    @IBOutlet var parisTitle: UILabel!
+    @IBOutlet var parisVisitor: UILabel!
+    @IBOutlet var parisLocation: UILabel!
+    @IBOutlet var parisDuration: UILabel!
+    @IBOutlet var parisDescription: UILabel!
+    @IBOutlet var parisButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let expositionInfo = loadInfo() else {
+            return
+        }
+        setInitValue(expositionInfo: expositionInfo)
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func loadInfo() -> ExpositionInfo? {
+        let jsonManager: JsonManagerable = JsonManager()
+        do {
+            let expositionInfo = try jsonManager.decodedExpositionInfo()
+            return expositionInfo
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    func setInitValue(expositionInfo: ExpositionInfo) {
+        parisTitle.text = expositionInfo.title
+        parisVisitor.text = "\(expositionInfo.visitors)"
+        parisLocation.text = expositionInfo.location
+        parisDuration.text = expositionInfo.duration
+        parisDescription.text = expositionInfo.description
+        
+    }
 }
-
